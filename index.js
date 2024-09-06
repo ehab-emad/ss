cd870a7493b175b4d05d8e3d8266ae604367df77
ehab-emad <ehabemad515@gmail.com>
2024-09-06 09:36:12 +0300

    fdkkkkkkkddd;isglltldllld

--- index.js
+++ index.js
@@ -1,47 +1,33 @@
 
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const multer = require('multer');
-
-
-
-
-
-const port = process.env.PORT || 3090;
-
+const multer  = require('multer')
+const port = process.env.PORT || 30001;
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/');
-  },
-  filename: function (req, file, cb) {
-    let data = new Date();
-    let images = "images/" + file.originalname;
-    req.body.images = images;
-    cb(null, images);
-  }
-});
+    destination: function (req, file, cb) {
+      cb(null, 'public/')
+    },
+    filename: function (req, file, cb) {
+     let data = new Date()
+     let images = "images/" +file.originalname
+     req.body.images=images
+      cb(null, images)
+    }
+  })
+  const bodyParser = multer({ storage: storage }).any()
+  server.use(bodyParser)
+server.post("/products",(req, res, next) => {
+  let data=new Date()
+  // req.body.createdAt=data.toIsostring()
 
-const bodyParser = multer({ storage: storage }).any();
-
-// استخدام middlewares و router قبل الاستماع للمنفذ
-
-server.use(bodyParser);
-
-server.post("/products", (req, res, next) => {
-  let data = new Date();
-  // req.body.createdAt = data.toISOString();
 
   // Continue to JSON Server router
-  next();
-});
+  next()
+})
 
-
-
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
 server.use(middlewares);
 server.use(router);
 server.listen(port)
